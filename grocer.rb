d5feef2147c82d659f7e9f944c0151aedce1375e
Tiffany Tang <tifftang22@gmail.com>
2019-10-03 22:35:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -29,14 +29,14 @@ def apply_coupons(cart, coupons)
   cart
 end
 
-def apply_clearance(cart) 
-  cart.each do |item, attributes| 
+def apply_clearance(cart)
+  cart.each do |item, attributes|
     if attributes[:clearance] == true 
       attribute_hash[:price] = (attribute_hash[:price] *
-      0.8).round(2) 
-    end 
-  end 
-cart 
+      0.8).round(2)
+    end
+  end
+cart
 end
 
 def checkout(cart, coupons)
