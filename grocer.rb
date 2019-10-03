73612cff61dd9192b6aa85ed23c41b0b3d02d5f9
Tiffany Tang <tifftang22@gmail.com>
2019-10-03 21:37:12 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -6,6 +6,7 @@ def consolidate_cart(cart)
       clean_cart[item][:count] ? clean_cart[item][:count] +=1 : new_cart[item[[:count] = 1]]
     end
   end
+  new_cart
 end
 
 def apply_coupons(cart, coupons)
