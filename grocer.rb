60ab149f2d87127fe3ac4a7467745c89162b7419
Tiffany Tang <tifftang22@gmail.com>
2019-10-03 21:56:03 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -10,7 +10,10 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon_hash|
+    item=coupon_hash[:item]
+
+    if !cart[item].nil? && cart[item][:count] >= coupon_hash[:num]
 end
 
 def apply_clearance(cart)
