f2607129ae8d06efa9f4a2269a952d6fe6ca2d9b
Tiffany Tang <tifftang22@gmail.com>
2019-10-03 22:44:55 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,6 +47,8 @@ def checkout(cart, coupons)
   clearance_cart.each do |item, attribute_hash|
     total += (attribute_hash[:price] * attribute_hash[:count])
   end
-  total = (total * 0.9) if total > 100
+  if total > 100
+    total = (total * 0.9)
+  end
   total
 end
